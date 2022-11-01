525363a3304711c991639ade9b48665e70f907fb
Ali Aljabri <aljabri55@gmail.com>
2022-11-01 12:04:51 +0300

    fix passing message

--- js/background.js
+++ js/background.js
@@ -1,5 +1,10 @@
 chrome.runtime.onMessage.addListener(message)
 
-function message(request){
-        console.log(request.cols);
+function message(request) {
+    chrome.runtime.sendMessage({
+        msg: "something_completed",
+        columns: request.columns,
+    });
 }
+
+
--- js/popup.js
+++ js/popup.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded',()=>{
+    var element = document.querySelector("#columns-length")
+    chrome.runtime.onMessage.addListener( (request, sender, sendResponse) => {
+
+        if (request.msg === "something_completed") {
+            //  To do something
+            console.log(request.columns)
+            if(element){
+                element.textContent =  `${request.columns}`
+            }
+
+        }
+    });
+
+
+} )
 
